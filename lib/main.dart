378a185a9ea02df12bad507975631be58c8f85d6
BabarKhan <babarkhan138@gmail.com>
2021-07-18 22:19:10 +0500

    Day 3 - routes, text styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+
+import 'pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      routes: {
+        '/': (context) => LoginPage(),
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
