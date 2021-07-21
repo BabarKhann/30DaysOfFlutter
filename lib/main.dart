f9595e328c4e49292bce46092138b1c129f17ee7
BabarKhan <babarkhan138@gmail.com>
2021-07-21 08:37:57 +0500

    worked with image, columns, SizedBox, Padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/login_page.dart';
-import 'pages/home_page.dart';
+// import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,10 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       routes: {
         '/': (context) => LoginPage(),
         '/login': (context) => LoginPage(),
       },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
